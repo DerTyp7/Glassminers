e65f73705cf2cb7bd4f03a893f2a99c920181319
surrealtm <matheke.victor@gmail.com>
2025-05-29 00:01:23 +0200

    Disabled socket lingering again in the virtual connection module.

--- run_tree/server_script.sh
+++ run_tree/server_script.sh
@@ -5,7 +5,7 @@ TIME=5
 
 while [ true ]; do
     $EXEC
-    echo "[WCHDOG][ Info] Server was stopped at $(date +"%d.%m.%Y %T")] with: $?"
+    echo "[WCHDOG][ Info] Server was stopped at [$(date +"%d.%m.%Y %T")] with: $?"
     echo "[WCHDOG][ Info] Restarting in $TIME seconds unless enter is pressed..."
     read -t $TIME input;
     if [ $? == 0 ]; then
