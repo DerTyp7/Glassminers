6e22659d78b91cf630c089bbcc885081ca043671
surrealtm <matheke.victor@gmail.com>
2025-06-14 12:48:41 +0200

    Updated the server script to put logs into actual files.

--- run_tree/server_script.sh
+++ run_tree/server_script.sh
@@ -3,10 +3,14 @@
 EXEC=./server.out
 TIME=5
 
+mkdir -p logs
+
 while [ true ]; do
-    $EXEC
-    echo "[WCHDOG][ Info] Server was stopped at [$(date +"%d.%m.%Y %T")] with: $?"
-    echo "[WCHDOG][ Info] Restarting in $TIME seconds unless enter is pressed..."
+    TIMESTAMP=$(date +"%d.%m.%Y %T")
+    echo "[WCHDOG][Info] Server is starting at [$TIMESTAMP] with: $?"
+    $EXEC > "logs/$TIMESTAMP.log"
+    echo "[WCHDOG][Info] Server stopped at [$(date +"%d.%m.%Y %T")] with: $?"
+    echo "[WCHDOG][Info] Restarting in $TIME seconds unless enter is pressed..."
     read -t $TIME input;
     if [ $? == 0 ]; then
         break;
